58ac81d8a3f5dcc5d51b0d1471c1c86cea60f89b
ViliusKk <vilius.kuokstas@gmail.com>
2024-12-15 14:46:19 +0200

    Enemy pathfind

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
@@ -8,12 +9,31 @@ public class Enemy : MonoBehaviour
     public int scareDistance = 3;
     public Transform target;
     public Image scaryImage;
+    NavMeshAgent agent;
+    public int viewDistance = 10;
 
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, viewDistance);
+    }
 
     void Update()
     {
+        //agent.SetDestination(target.position);
+
         float distance = Vector3.Distance(transform.position, target.position);
 
+
+        if (distance <= viewDistance)
+        {
+            agent.SetDestination(target.position);
+        }
         if (distance < scareDistance)
         {
             Jumpscare();
